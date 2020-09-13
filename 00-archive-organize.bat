f4eeb178e5e81b90233f74bdc63c26cd6a6d9f5c
felipearc13 <felipesilva1302@gmail.com>
2020-09-13 16:46:30 -0300

    Create 00-archive-organize.bat

---
+++ 00-archive-organize.bat
@@ -0,0 +1,67 @@
+@echo off
+
+mkdir Arquivos Torrent Audio Documents Programas Pictures Videos Compactados ISO Fontes
+
+move *mp3 Audio
+move *m4a Audio
+
+move *txt Documents
+move *vsdx Documents
+move *doc Documents
+move *docx Documents
+move *txt Documents
+move *pdf Documents
+move *txt Documents
+move *log Documents
+move *html Documents
+move *mhtml Documents
+move *epub Documents
+move *azw3 Documents
+move *azw Documents
+move *mobi Documents
+move *xlsx Documents
+move *xls Documents
+move *pkt Documents
+move *pptx Documents
+move *ppt Documents
+move *odp Documents
+move *xml Documents
+move *csv Documents
+
+move *exe Programas
+move *msi Programas
+move *vbox-extpack Programas
+move *reg Programas
+move *ps1 Programas
+move *vbs Programas
+move *sh Programas
+
+move *jpg Pictures
+move *jpeg Pictures
+move *png Pictures
+move *jfif Pictures
+
+move *srt Videos
+move *mkv Videos
+move *mp4 Videos
+move *avi Videos
+
+move *rar Compactados
+move *ova Compactados
+move *zip Compactados
+move *xz Compactados
+move *gz Compactados
+move *img Compactados
+move *iso ISO
+move *vdi ISO
+
+move *ttf Fontes 
+
+move *torrent Torrent
+
+echo FIM...
+
+
+
+
+exit
